d77e77d3b99166baf2563daf344b84de42f10732
ankitchauhan9090 <114400704+ankitchauhan9090@users.noreply.github.com>
2022-12-23 17:05:03 +0530

    Initial commit

---
+++ project1.py
@@ -0,0 +1,68 @@
+import sys
+import cv2
+import os
+from cvzone.HandTrackingModule import HandDetector
+ 
+
+def getImg(args):
+    if len(args) == 2:
+        if os.path.exists(args[1]):
+            return args[1]
+    return "ik1.png"
+    
+photo = getImg(sys.argv)
+
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+detector = HandDetector(detectionCon=0.8)
+startDist = None
+scale = 0
+cx, cy = 500,500
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img)
+    print("photo",photo)
+    img1 = cv2.imread(photo)
+    if len(hands)==2:
+        #print(detector.fingersUp(hands[0]),detector.fingersUp(hands[1]))
+        if detector.fingersUp(hands[0])==[1,1,0,0,0] and detector.fingersUp(hands[1])==[1,1,0,0,0]:
+            #print("Zoom Gesture")
+            lmList1 =  hands[0]['lmList']
+            lmList2 =  hands[1]['lmList']
+            #point 8 is the tip of the index finger.
+            if startDist is None:
+                #length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
+                length, info , img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+                startDist = length
+            #length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
+            length, info , img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+           
+            scale = int(length - startDist) //2
+            cx,cy =info[4:]
+            print(scale)
+    elif len(hands)==1 :
+        if detector.fingersUp(hands[0]) == [0,1,0,0,0]:
+            cx , cy = hands[0]['center']
+            print(cx, cy)
+
+    else:
+
+        startDist = None
+    try:
+        h1, w1, _= img1.shape
+        newH,newW = ((h1 + scale)//2)*2, ((w1 + scale)//2)*2
+        img1 = cv2.resize(img1, (newW, newH))
+        img[cy-newH//2:cy+ newH//2,cx-newW//2:cx+newW//2] = img1
+    except:
+        pass
+    cv2.imshow("Image",img)
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows()
+cap.release()
+
+    
